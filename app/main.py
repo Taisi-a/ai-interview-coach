970a211f0aa5c64e9c1dc32af6956b52eaecc957
Дмитриев Родион Сергеевич <rsdmitriev@edu.hse.ru>
2026-02-28 15:27:41 +0300

    added dockerfile

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@
 from fastapi import FastAPI
 from app.database import engine, Base
 from app.routers import auth, resume, chat, session
-
+from fastapi.middleware.cors import CORSMiddleware
 # Создаём таблицы
 Base.metadata.create_all(engine)
 
@@ -11,7 +11,7 @@ app = FastAPI()
 # CORS — разрешаем фронту стучаться в API
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[FRONTEND_URL],  # адрес фронта из .env
+    allow_origins=["*"],  # адрес фронта из .env
     allow_credentials=True,
     allow_methods=["*"],           # разрешаем GET, POST, PATCH и т.д.
     allow_headers=["*"],           # разрешаем любые заголовки (включая Authorization)
